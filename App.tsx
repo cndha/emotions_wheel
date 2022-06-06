ae26a1931c5d1eb9b78a67bf664d8ce94e4f0fb8
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-06-06 18:57:43 -0400

    configure prettier

--- App.tsx
+++ App.tsx
@@ -1,26 +1,25 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, FlatList } from 'react-native';
-import FeelingItem from './components/FeelingItem'
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, FlatList} from 'react-native';
+import FeelingItem from './components/FeelingItem';
 
 const App = () => {
-
-  return(
+  return (
     <View style={styles.container}>
       <Text style={styles.title}>Tap a feeling to learn more</Text>
-      <FeelingItem/>
+      <FeelingItem />
     </View>
-  )
-}
+  );
+};
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     padding: 100,
   },
   title: {
     fontSize: 30,
     textAlign: 'center',
   },
-})
+});
