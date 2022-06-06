0cd93073e34fce6e824ca06766d299266bde3c93
Cindy Ha <cindyhaa@gmail.com>
2022-06-06 15:10:03 -0700

    components

---
+++ components/HoverItem.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native'
+
+const HoverItem = () => {
+    return (
+        <View>
+            <Text>Hover element</Text>
+        </View>
+    )
+};
+
+export default HoverItem;
